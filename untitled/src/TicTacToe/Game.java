fb0b55c61e80846691435e8131d971e3d0cce1aa
RadekBronka <radoslavb12@gmail.com>
2024-11-17 15:15:58 +0100

    Update Game.java

--- untitled/src/TicTacToe/Game.java
+++ untitled/src/TicTacToe/Game.java
@@ -1,13 +1,52 @@
 package TicTacToe;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.random.*;
 
 public class Game {
+    boolean player1Starts;
     Board board;
     Player player1;
     Player player2;
 
     Game(){
+
         this.board = new Board();
-        this.player1=new Player('x');
-        this.player2=new Player('o');
+        this.player1=new Player('X');
+        this.player2=new Player('O');
+    }
+    void play(){Scanner scanner = new Scanner(System.in);
+       board.printBoard();
+        while(true) {
+            System.out.println("Player 1's turn");
+            boolean player1placed;
+            do {
+                int row1 = scanner.nextInt();
+                int col1 = scanner.nextInt();
+                player1placed = board.setBoard(row1, col1, player1);
+            } while (!player1placed);
+            board.printBoard();
+            if (player1.checkWin()){
+                System.out.println("Player 1 won");
+            break;
+            }
+            if (board.checkDraw()){
+                System.out.println("Draw");
+                break;
+            }
+            System.out.println("Player 2's turn");
+            boolean player2placed;
+            do{
+                int row1 = scanner.nextInt();
+                int col1 = scanner.nextInt();
+                player2placed=board.setBoard(row1,col1,player2);
+            }while(!player2placed);
+            board.printBoard();
+            if (player2.checkWin()){
+                System.out.println("Player 2 won");
+                break;
+            }
+
+        }
     }
 }
