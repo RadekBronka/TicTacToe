7b6b185c27d68a92fe6fb45b4f2dd151adcca733
RadekBronka <radoslavb12@gmail.com>
2024-11-17 15:15:41 +0100

    Update Board.java

--- untitled/src/TicTacToe/Board.java
+++ untitled/src/TicTacToe/Board.java
@@ -2,8 +2,14 @@ package TicTacToe;
 
 public class Board {
     char[][] board;
+    int[][] winBoard;
     Board(){
         board = new char[3][3];
+        winBoard = new int[][]{
+                {4, 9, 2},
+                {3, 5, 7},
+                {8, 1, 6}
+        };
     }
     void printBoard(){
         for (int i = 0; i < 3; i++) {
@@ -13,7 +19,30 @@ public class Board {
             System.out.println();
         }
     }
-    void setBoard(int x, int y,char player){
-        board[x][y] = player;
+    void printWinBoard(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(winBoard[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    boolean setBoard(int x, int y,Player player){
+        if ( board[x-1][y-1]==0) {
+            board[x-1][y-1] = player.symbol;
+            player.winCondition[x-1][y-1]=winBoard[x-1][y-1];
+            return true;
+        }
+        return false;
+    }
+    boolean checkDraw(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j]==0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
